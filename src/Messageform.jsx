0a8a5b3d0d5ace3fa9b663e4b92b2c12c763ee5e
Rebecca <rebock@omegapoint.se>
2018-10-30 13:01:30 +0100

    first commit

---
+++ src/Messageform.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import firebase from './fire';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import * as contentful from 'contentful'
+
+class MessageForm extends Component {
+constructor(props) {
+  super(props);
+    this.state = { 
+      val: ""
+    }
+}
+
+handleChange = (e) => {
+    this.setState({val: e.target.value});
+}
+
+handleSubmit = () => {
+    const msg = this.state.val;
+    firebase.database().ref('messages/').push({
+        msg
+    }).then((data)=>{
+        //success callback
+        console.log('data ' , data)
+    }).catch((error)=>{
+        //error callback
+        console.log('error ' , error)
+    })
+}
+
+render() {
+    return (
+        <div>
+            <form>
+            <TextField onChange={this.handleChange}/>   
+            <Button onClick={this.handleSubmit}>Submit</Button>   
+            </form>
+        </div>
+
+    );
+  }
+}
+
+export default MessageForm;
---
+++ src/fire.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+var config = {
+    apiKey: "AIzaSyA8KMyz4QmdFZiPp52LbNXNnXCaB4uLsKQ",
+    authDomain: "magicmirror-383ed.firebaseapp.com",
+    databaseURL: "https://magicmirror-383ed.firebaseio.com",
+    projectId: "magicmirror-383ed",
+    storageBucket: "magicmirror-383ed.appspot.com",
+    messagingSenderId: "583861133769"
+  };
+  export default firebase.initializeApp(config);
