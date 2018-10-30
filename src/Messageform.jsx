5f26b1142454335efc44cb4495b55b7ac55a3f2b
Rebecca <rebock@omegapoint.se>
2018-10-30 13:45:05 +0100

    config changes

--- src/Messageform.jsx
+++ src/Messageform.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import firebase from './fire';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import * as contentful from 'contentful'
 
 class MessageForm extends Component {
 constructor(props) {
